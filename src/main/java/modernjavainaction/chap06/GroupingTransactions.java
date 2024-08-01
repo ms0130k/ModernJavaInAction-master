aca6338c38c339bf5bd3d4c155f1650b048edf48
KimDonghyeon <minseok0130@gmail.com>
2024-08-01 23:54:40 +0900

    collect Collector

--- src/main/java/modernjavainaction/chap06/GroupingTransactions.java
+++ src/main/java/modernjavainaction/chap06/GroupingTransactions.java
@@ -2,83 +2,125 @@ package modernjavainaction.chap06;
 
 import static java.util.stream.Collectors.groupingBy;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class GroupingTransactions {
 
-  public static List<Transaction> transactions = Arrays.asList(
-    new Transaction(Currency.EUR, 1500.0),
-    new Transaction(Currency.USD, 2300.0),
-    new Transaction(Currency.GBP, 9900.0),
-    new Transaction(Currency.EUR, 1100.0),
-    new Transaction(Currency.JPY, 7800.0),
-    new Transaction(Currency.CHF, 6700.0),
-    new Transaction(Currency.EUR, 5600.0),
-    new Transaction(Currency.USD, 4500.0),
-    new Transaction(Currency.CHF, 3400.0),
-    new Transaction(Currency.GBP, 3200.0),
-    new Transaction(Currency.USD, 4600.0),
-    new Transaction(Currency.JPY, 5700.0),
-    new Transaction(Currency.EUR, 6800.0)
-  );
+    public static List<Transaction> transactions = Arrays.asList(
+            new Transaction(Currency.EUR, 1500.0),
+            new Transaction(Currency.USD, 2300.0),
+            new Transaction(Currency.GBP, 9900.0),
+            new Transaction(Currency.EUR, 1100.0),
+            new Transaction(Currency.JPY, 7800.0),
+            new Transaction(Currency.CHF, 6700.0),
+            new Transaction(Currency.EUR, 5600.0),
+            new Transaction(Currency.USD, 4500.0),
+            new Transaction(Currency.CHF, 3400.0),
+            new Transaction(Currency.GBP, 3200.0),
+            new Transaction(Currency.USD, 4600.0),
+            new Transaction(Currency.JPY, 5700.0),
+            new Transaction(Currency.EUR, 6800.0)
+    );
 
-  public static void main(String... args) {
-    groupImperatively();
-    groupFunctionally();
-  }
+    public static void main(String... args) {
+//    groupImperatively();
+//    groupFunctionally();
+//    Map<Currency, List<Transaction>> currencyListMap = groupByMine();
+//    System.out.println(currencyListMap);
 
-  private static void groupImperatively() {
-    Map<Currency, List<Transaction>> transactionsByCurrencies = new HashMap<>();
-    for (Transaction transaction : transactions) {
-      Currency currency = transaction.getCurrency();
-      List<Transaction> transactionsForCurrency = transactionsByCurrencies.get(currency);
-      if (transactionsForCurrency == null) {
-        transactionsForCurrency = new ArrayList<>();
-        transactionsByCurrencies.put(currency, transactionsForCurrency);
-      }
-      transactionsForCurrency.add(transaction);
+        Map<Currency, List<Transaction>> currencyListMap = groupByMine();
+        System.out.println(currencyListMap);
+        currencyListMap = groupByMyStream();
+        System.out.println(currencyListMap);
+
     }
 
-    System.out.println(transactionsByCurrencies);
-  }
-
-  private static void groupFunctionally() {
-    Map<Currency, List<Transaction>> transactionsByCurrencies = transactions.stream()
-        .collect(groupingBy(Transaction::getCurrency));
-    System.out.println(transactionsByCurrencies);
-  }
-
-  public static class Transaction {
-
-    private final Currency currency;
-    private final double value;
-
-    public Transaction(Currency currency, double value) {
-      this.currency = currency;
-      this.value = value;
+    private static Map<Currency, List<Transaction>> groupByMyStream() {
+        List<Transaction> trs = generateTransactions();
+        return trs.stream()
+//                .collect(groupingBy(Transaction::getCurrency));
+                .collect(groupingBy(Transaction::getCurrency));
     }
 
-    public Currency getCurrency() {
-      return currency;
+    private static Map<Currency, List<Transaction>> groupByMine() {
+        Map<Currency, List<Transaction>> trsByCurrency = new HashMap<>();
+        List<Transaction> trs = generateTransactions();
+        for (Transaction tr : trs) {
+            List<Transaction> currTrs = trsByCurrency.get(tr.getCurrency());
+            if (currTrs == null) {
+                currTrs = new ArrayList();
+                trsByCurrency.put(tr.getCurrency(), currTrs);
+            }
+            currTrs.add(tr);
+        }
+        return trsByCurrency;
     }
 
-    public double getValue() {
-      return value;
+    private static List<Transaction> generateTransactions() {
+        return Arrays.asList(
+                new Transaction(Currency.EUR, 1500.0),
+                new Transaction(Currency.USD, 2300.0),
+                new Transaction(Currency.GBP, 9900.0),
+                new Transaction(Currency.EUR, 1100.0),
+                new Transaction(Currency.JPY, 7800.0),
+                new Transaction(Currency.CHF, 6700.0),
+                new Transaction(Currency.EUR, 5600.0),
+                new Transaction(Currency.USD, 4500.0),
+                new Transaction(Currency.CHF, 3400.0),
+                new Transaction(Currency.GBP, 3200.0),
+                new Transaction(Currency.USD, 4600.0),
+                new Transaction(Currency.JPY, 5700.0),
+                new Transaction(Currency.EUR, 6800.0));
     }
 
-    @Override
-    public String toString() {
-      return currency + " " + value;
+    private static void groupImperatively() {
+        Map<Currency, List<Transaction>> transactionsByCurrencies = new HashMap<>();
+        for (Transaction transaction : transactions) {
+            Currency currency = transaction.getCurrency();
+            List<Transaction> transactionsForCurrency = transactionsByCurrencies.get(currency);
+            if (transactionsForCurrency == null) {
+                transactionsForCurrency = new ArrayList<>();
+                transactionsByCurrencies.put(currency, transactionsForCurrency);
+            }
+            transactionsForCurrency.add(transaction);
+        }
+
+        System.out.println(transactionsByCurrencies);
     }
 
-  }
+    private static void groupFunctionally() {
+        Map<Currency, List<Transaction>> transactionsByCurrencies = transactions.stream()
+                .collect(groupingBy(Transaction::getCurrency));
+        System.out.println(transactionsByCurrencies);
+    }
 
-  public enum Currency {
-    EUR, USD, JPY, GBP, CHF
-  }
+    public static class Transaction {
+
+        private final Currency currency;
+        private final double value;
+
+        public Transaction(Currency currency, double value) {
+            this.currency = currency;
+            this.value = value;
+        }
+
+        public Currency getCurrency() {
+            return currency;
+        }
+
+        public double getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return currency + " " + value;
+        }
+
+    }
+
+    public enum Currency {
+        EUR, USD, JPY, GBP, CHF
+    }
 
 }
--- src/main/java/modernjavainaction/chap06/Reducing.java
+++ src/main/java/modernjavainaction/chap06/Reducing.java
@@ -1,31 +1,79 @@
 package modernjavainaction.chap06;
 
-import static java.util.stream.Collectors.reducing;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.*;
 import static modernjavainaction.chap06.Dish.menu;
 
 public class Reducing {
 
-  public static void main(String... args) {
-    System.out.println("Total calories in menu: " + calculateTotalCalories());
-    System.out.println("Total calories in menu: " + calculateTotalCaloriesWithMethodReference());
-    System.out.println("Total calories in menu: " + calculateTotalCaloriesWithoutCollectors());
-    System.out.println("Total calories in menu: " + calculateTotalCaloriesUsingSum());
-  }
+    public static void main(String... args) {
+        Long collect = menu.stream().collect(counting());
 
-  private static int calculateTotalCalories() {
-    return menu.stream().collect(reducing(0, Dish::getCalories, (Integer i, Integer j) -> i + j));
-  }
+//        long count = menu.stream().count();
+//        System.out.println(count);
+//        Optional<Dish> collect = menu.stream().collect(maxBy(comparing(Dish::getCalories)));
+//        System.out.println(collect.get());
+//
+//        System.out.println(menu.stream().collect(minBy(comparing(Dish::getCalories))).get());
+//        System.out.println(menu.stream().collect(summingInt(Dish::getCalories)));
+//        System.out.println(menu.stream().map(Dish::getCalories).reduce(0, Integer::sum));
+//        System.out.println(menu.stream().collect(averagingInt(Dish::getCalories)));
+//        IntSummaryStatistics collect1 = menu.stream().collect(summarizingInt(Dish::getCalories));
+//        System.out.println(collect1);
+//        System.out.println(menu.stream().map(Dish::getName).collect(joining(", ")));
 
-  private static int calculateTotalCaloriesWithMethodReference() {
-    return menu.stream().collect(reducing(0, Dish::getCalories, Integer::sum));
-  }
+//        List<Dish> dishes = List.of(
+//                new Dish("mydish", false, 100, Dish.Type.MEAT),
+//                new Dish("2mydish", false, 100, Dish.Type.MEAT)
+//        );
+//        Optional<Dish> collect2 = dishes.stream()
+//                .collect(reducing((prev, curr) -> {
+//                    System.out.println("prev: " + prev);
+//                    System.out.println("curr: " + curr);
+//                    return prev.getCalories() > curr.getCalories() ? prev : curr;
+//                }));
+//        Dish dish = collect2.get();
+//        System.out.println(dish);
+//
+//        String collect1 = dishes.stream().map(Dish::getName).collect(joining());
+//        System.out.println(collect1);
 
-  private static int calculateTotalCaloriesWithoutCollectors() {
-    return menu.stream().map(Dish::getCalories).reduce(Integer::sum).get();
-  }
+        Map<Grouping.CaloricLevel, List<Dish>> collect1 = menu.stream()
+                .collect(groupingBy(d -> {
+                    if (d.getCalories() < 200) {
+                        return Grouping.CaloricLevel.DIET;
+                    } else if (d.getCalories() < 400) {
+                        return Grouping.CaloricLevel.NORMAL;
+                    }
+                    return Grouping.CaloricLevel.FAT;
+                }));
+        System.out.println(collect1);
 
-  private static int calculateTotalCaloriesUsingSum() {
-    return menu.stream().mapToInt(Dish::getCalories).sum();
-  }
+
+
+//    System.out.println("Total calories in menu: " + calculateTotalCalories());
+//    System.out.println("Total calories in menu: " + calculateTotalCaloriesWithMethodReference());
+//    System.out.println("Total calories in menu: " + calculateTotalCaloriesWithoutCollectors());
+//    System.out.println("Total calories in menu: " + calculateTotalCaloriesUsingSum());
+    }
+
+    private static int calculateTotalCalories() {
+        return menu.stream().collect(reducing(0, Dish::getCalories, (Integer i, Integer j) -> i + j));
+    }
+
+    private static int calculateTotalCaloriesWithMethodReference() {
+        return menu.stream().collect(reducing(0, Dish::getCalories, Integer::sum));
+    }
+
+    private static int calculateTotalCaloriesWithoutCollectors() {
+        return menu.stream().map(Dish::getCalories).reduce(Integer::sum).get();
+    }
+
+    private static int calculateTotalCaloriesUsingSum() {
+        return menu.stream().mapToInt(Dish::getCalories).sum();
+    }
 
 }
