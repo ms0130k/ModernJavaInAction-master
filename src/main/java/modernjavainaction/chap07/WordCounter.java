f33304c4da0bacef2b0be8bff87bf0c0f151ba12
KimDonghyeon <minseok0130@gmail.com>
2024-08-22 07:52:30 +0900

    forkjoin framework 실습

--- src/main/java/modernjavainaction/chap07/WordCounter.java
+++ src/main/java/modernjavainaction/chap07/WordCounter.java
@@ -1,97 +1,46 @@
 package modernjavainaction.chap07;
 
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.ForkJoinTask;
-import java.util.concurrent.RecursiveTask;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.LongStream;
-import java.util.stream.Stream;
 
 import static modernjavainaction.chap07.WordCount.SENTENCE;
 
 public class WordCounter {
-    private final int counter;
-    private final boolean lastSpace;
-
-    public WordCounter(int counter, boolean lastSpace) {
-        this.counter = counter;
-        this.lastSpace = lastSpace;
-    }
-
-    public WordCounter accumulate(Character c) {
-        if (Character.isWhitespace(c)) {
-            return lastSpace ?
-                    this :
-                    new WordCounter(counter, true);
+    public static int countWordIteratively(String s) {
+        char[] chars = s.toCharArray();
+        int result = 0;
+        boolean prevIsSpace = true;
+        for (char c : chars) {
+            if (Character.isWhitespace(c)) {
+                prevIsSpace = true;
+                continue;
+            }
+            if (prevIsSpace) result++;
+            prevIsSpace = false;
         }
-        return lastSpace ?
-                new WordCounter(counter + 1, false) :
-                this;
-    }
-
-    public WordCounter combine(WordCounter wordCounter) {
-        return new WordCounter(counter + wordCounter.counter,
-                wordCounter.lastSpace);
-    }
-
-    public int getCounter() {
-        return counter;
-    }
-
-    public static int countWords(Stream<Character> stream) {
-        return stream.reduce(
-                new WordCounter(0, true),
-                WordCounter::accumulate,
-                WordCounter::combine
-        ).getCounter();
+        return result;
     }
 
     public static void main(String[] args) {
-        ForkJoinPool pool = new ForkJoinPool();
-        long[] numbers = LongStream.rangeClosed(1, 1000000).toArray();
-        Long result = pool.invoke(new ForkJoinSumCalculator(numbers));
-        System.out.println(result);
+        int i = countWordIteratively(SENTENCE);
+        assert i == 19;
+        long start = System.nanoTime();
+        int iterationCount = 1000000;
+        for (int j = 0; j < iterationCount; j++) {
+            List<Character> collect = SENTENCE.chars()
+                    .mapToObj(c -> (char) c)
+                    .collect(Collectors.toList());
+        }
+        System.out.println((System.nanoTime() - start) / 1_000_000);
+
+        start = System.nanoTime();
+        for (int j = 0; j < iterationCount; j++) {
+            List<Character> collect1 = IntStream.range(0, SENTENCE.length())
+                    .mapToObj(SENTENCE::charAt)
+                    .collect(Collectors.toList());
+        }
+        System.out.println((System.nanoTime() - start) / 1_000_000);
     }
-
-    static class ForkJoinSumCalculator extends RecursiveTask<Long> {
-        private final long[] numbers;
-        private final int start;
-        private final int end;
-        private final long THRESHOLD = 10_000;
-
-        public ForkJoinSumCalculator(long[] numbers) {
-            this(numbers, 0, numbers.length);
-        }
-
-        public ForkJoinSumCalculator(long[] numbers, int start, int end) {
-            this.numbers = numbers;
-            this.start = start;
-            this.end = end;
-        }
-
-        @Override
-        protected Long compute() {
-            int length = end - start;
-            if (THRESHOLD >= length) {
-                return computeSequentially();
-            }
-
-            int mid = (start + end) / 2;
-            ForkJoinSumCalculator leftTask = new ForkJoinSumCalculator(numbers, start, mid);
-            leftTask.fork();
-            ForkJoinSumCalculator rightTask = new ForkJoinSumCalculator(numbers, mid, end);
-            Long rightResult = rightTask.compute();
-            Long leftResult = leftTask.join();
-            return rightResult + leftResult;
-        }
-
-        private long computeSequentially() {
-            long result = 0;
-            for (int i = start; i < end; i++) {
-                result += numbers[i];
-            }
-            return result;
-        }
-    }
-
 }
